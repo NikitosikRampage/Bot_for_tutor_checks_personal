020d378e3378813c45ea50d441516ce90592864c
NikitosikRampage <nikitadem2006@gmail.com>
2026-02-19 22:44:10 +0300

    Фикс <b>

--- main.py
+++ main.py
@@ -374,8 +374,8 @@ async def show_all_payments(message: Message):
                 )
                 lines.append(line)
 
-            lines.append(f"<b>Итого: {total_to_pay:,.0f} ₽ к выплате\n"
-                         f"───────────────")
+            lines.append(f"<b>Итого: </b>{total_to_pay:,.0f} ₽ к выплате\n"
+                         f"───────────────\n")
             text = "───────────────\n".join(lines)
 
             await message.answer(text, parse_mode="HTML")
